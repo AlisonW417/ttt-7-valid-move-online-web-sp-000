c4ad79129a56cc76451b8e12bb64ea3e74a36991
Alison Williams <apw5011@gmail.com>
2020-10-30 03:04:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index] != " " && board[index] != "" && board[index] != nil
-    puts "The potision must be vacant, not currently taken by a player"
-  elsif board[index] == " " && board[index] == "" && board[index] == nil 
-  elsif board[index].to_i >= 0 && board[index].to_i <= 8 
-  else
-  end
+  board[index].to_i >= 0 && board[index].to_i <= 8
+  board[index] != " " && board[index] != "" && board[index] != nil
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
