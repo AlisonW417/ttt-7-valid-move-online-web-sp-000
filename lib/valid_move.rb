3f9a8f00af06f66150bded041a1b3bdc32134f3e
Alison Williams <apw5011@gmail.com>
2020-10-30 02:23:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
- if board[index].to_i < 0 || board[index].to_i > 8
-   puts "You must move to a position within the tic-tac-toe board"
- elsif board[index] != " " && board[index] != "" && board[index] != nil
+ if board[index] != " " && board[index] != "" && board[index] != nil
    puts "The position must be vacant, not currently taken by a player"
-  else board[index].to_i.between?(0,8)
+  else board[index].to_i >= 0 && board[index].to_i <= 8
   end
 end
 
