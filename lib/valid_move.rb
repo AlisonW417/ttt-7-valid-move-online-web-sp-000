43a0face752d69f7c8e85b151b5ffa3f322e471d
Alison Williams <apw5011@gmail.com>
2020-10-30 02:13:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index] != " " && board[index] != "" && board[index] != nil
+ if board[index].to_i < 0 || board[index].to_i > 8
+   puts "you must "
+ if board[index] != " " && board[index] != "" && board[index] != nil
   else board[index].to_i.between?(0,8)
   end
 end
