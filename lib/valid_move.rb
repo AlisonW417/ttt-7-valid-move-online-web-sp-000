db80fc1b531cebe9374d20a7fd3a5c06fa2db435
Alison Williams <apw5011@gmail.com>
2020-10-30 02:32:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,7 @@ def valid_move?(board, index)
  if board[index] != " " && board[index] != "" && board[index] != nil
    puts "The position must be vacant, not currently taken by a player"
   else board[index].to_i >= 0 && board[index].to_i <= 8
+  
   end
 end
 
